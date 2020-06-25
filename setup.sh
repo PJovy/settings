4a53aabf0dc3c46160139c7febea51ded9850354
PJovy <pengjovy@gmail.com>
2020-06-25 20:06:13 +0800

    Choose different package manager according OS version.

--- setup.sh
+++ setup.sh
@@ -3,35 +3,53 @@
 echo "Please run this shell script at sudo."
 echo "Now your are using $(echo $SHELL)"
 
-echo "########### APT Install #########"
-apt update -y
-echo "Installing zsh, vim, wget ,git..."
-apt install zsh vim wget git 
-echo "Apt install finished."
-echo "########## Updating APT ##########"
 
-echo "##########Adding git aliases##########"
+echo "##########     Welcome to Jovy's setup script!     ##########"
+
+echo "##########     Your current shell: $SHELL     ##########"
+
+if [[ $(uname) = "Darwin" ]]; then
+  echo "You are using Mac."
+  echo "########### brew install #########"
+  brew update -y
+  echo "installing zsh, vim, wget ,git..."
+  brew install zsh vim wget git 
+  echo "brew install finished."
+  echo "########## brew install finished ##########"
+else
+  echo "You are using Linux"
+  echo "########### apt install #########"
+  apt update -y
+  echo "installing zsh, vim, wget ,git..."
+  apt install zsh vim wget git 
+  echo "apt install finished."
+  echo "########## apt install finished ##########"
+fi
+
+
+
+echo "##########  adding git aliases  ##########"
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
-git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global alias.lg "log --color --graph --pretty=format:'%cred%h%creset -%c(yellow)%d%creset %s %cgreen(%cr) %c(bold blue)<%an>%creset' --abbrev-commit"
 echo "git aliases setting finished."
 sleep 2
-echo "##########Adding git aliases##########"
+echo "##########  adding git aliases finished  ##########"
 
 
-echo "###############  Setting up ohmyzsh  ###############"
-echo "Now downloading omyzsh framework"
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+echo "###############  setting up ohmyzsh  ###############"
+echo "now downloading oh-my-zsh framework"
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -o - | zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
-echo "Changing default theme to ys:"
-sed -ie 's/^ZSH_THEME.*/ZSH_THEME="ys"/g' ~/.zshrc
+echo "changing default theme to ys:"
+sed -ie 's/^zsh_theme.*/zsh_theme="ys"/g' ~/.zshrc
 
-echo "Downloading and enabling Oh-my-zsh plugins"
-FILE1=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-FILE2=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+echo "downloading and enabling oh-my-zsh plugins"
+file1=${zsh_custom:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+file2=${zsh_custom:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 echo $FILE1
 echo $FILE2
@@ -50,7 +68,7 @@ fi
 
 sed -ie 's/^plugins=.*/plugins=(vi-mode git docker zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc 
 source ~/.zshrc
-echo "###############  Setting up ohmyzsh  ###############"
+echo "###############  Setting up ohmyzsh finished  ###############"
 
 
 echo "Changing shell to zsh"
