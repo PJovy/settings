37ab430f5d485f8a21eb3a66c7d2bc8d1e711b11
PJovy <pengjovy@gmail.com>
2020-06-25 11:32:55 +0800

    Rename and update setup.sh

---
+++ setup.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+echo "Please run this shell script at sudo."
+echo "Now your are using $(echo $SHELL)"
+
+echo "Update apt...."
+apt update -y
+
+echo "Installing zsh, vim, wget ,git..."
+apt install zsh vim wget git 
+echo "Apt install finished."
+sleep 2
+
+echo "Changing shell to zsh"
+chsh -s /usr/bin/zsh
+echo "Now you are using: $SHELL"
+sleep 2
+
+echo "##########Adding git aliases##########"
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+echo "git aliases setting finished."
+sleep 2
+
+
+echo "###############  Setting up ohmyzsh  ###############"
+echo "Now downloading omyzsh framework"
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+
+echo "Changing default theme to ys:"
+sed -ie 's/^ZSH_THEME.*/ZSH_THEME="ys"/g' ~/.zshrc
+
+echo "Downloading and enabling Oh-my-zsh plugins"
+FILE1=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+FILE2=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+echo $FILE1
+echo $FILE2
+
+if [[ -d "$FILE1" ]]; then
+    echo "zsh-autosuggestions exist, won't download."
+else
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
+
+if [[ -d "$FILE2" ]]; then                                                                                                                             
+    echo "syntax-highlighting exists, won't download."
+else
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+
+sed -ie 's/^plugins=.*/plugins=(git docker zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc 
+source ~/.zshrc
+
+echo "####################"
+echo "Setup finished, enjoy it~"
+echo "####################"
